48d31f67a8bba9ef4075e779433e6a8f7285b2bc
idej <idej.wrk@gmail.com>
2015-12-18 01:25:17 +0300

    add planes seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+15.times do |i|
+  Plane.create(name: ([i, '_'] + (0...10).map { ('a'..'z').to_a[rand(26)] }).join)
+end
