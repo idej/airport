bf05ee6e8350a6cd34e8c676ff8915ab11fe4e60
idej <idej.wrk@gmail.com>
2015-12-18 00:44:37 +0300

    set sidekiq as active job background

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module AirportTest
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    #Use sidekiq as active job backend
+    config.active_job.queue_adapter = :sidekiq
   end
 end
